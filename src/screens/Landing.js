b3a9dfb4fe20a4c7bf6caaf1fbf06830bcd25709
Owen Roth <rothowen27@gmail.com>
2021-02-02 15:28:22 -0600

    alert

--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -43,7 +43,10 @@ const Landing = () => {
                 ticking={true}
                 timezone={"US/Eastern"}
               />
-              {/* <Alert color="secondary">Pre-Market Hours</Alert> */}
+              <Alert color="secondary">
+                After-Market Hours: If "Not-found" is displayed. Market data has
+                stopped and will resume around 4am ET
+              </Alert>
             </h6>
             {/* <p style={{ textAlign: "center" }}>
               *Note: Prices remain 0 until market opens. EST
